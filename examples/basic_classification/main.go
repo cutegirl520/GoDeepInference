0edd40d20240980aa56c63e57609afe794201dc1
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2022-10-18 19:10:02 +0000

    rtlwifi: rtl8192cu: Change buffer allocation for synchronous reads

--- examples/basic_classification/main.go
+++ examples/basic_classification/main.go
@@ -9,4 +9,6 @@ import (
 
 const (
 	model      = "/CaffeNet.xml"
-	pluginPath = "/opt/intel/deep_learning_sdk_2016.1.0.861/deployment_tools/
+	pluginPath = "/opt/intel/deep_learning_sdk_2016.1.0.861/deployment_tools/inference_engine/lib/intel64/"
+	plugin     = "MKLDNNPlugin"
+	labelsFile = "/CaffeN
